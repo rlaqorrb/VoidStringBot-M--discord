f158c7712a6b3a79b7b1348a1aaca13ade1f2b9a
rlaqorrb <80166091+rlaqorrb@users.noreply.github.com>
2021-04-15 10:08:18 +0900

    Update VStr.py

--- VStr.py
+++ VStr.py
@@ -25,6 +25,17 @@ intro6 = "4. 여유가 있을 때엔 함께 대회참가를 한다."
 director = "부장(조윤성)"
 leader = "대장(유가은)"
 members = "```cs\n#1학년\n1. 10128 최의경\n2. 10203 김나영\n3. 10220 이현주\n4. 10309 김세은\n5. 10519 유가은[대장]\n6. 10608 김봉준\n7. 10609 김신후\n8. 10716 신유성\n9. 10822 이영준\n10. 10905 김민수\n11. 10907 김성욱\n12. 10913 두지훈\n13. 10914 문민성\n14. 10919 박준영\n15. 10920 배태환\n16. 10928 조윤성[부장]\n17. 11005 권해찬\n18. 11006 김건하\n19. 11011 김백규\n20. 11015 박진석\n21. 11016 신성훈```"
+announcement2 = """
+2021/04/07(수)/6교시
+```fix
+온라인 수업 2차시 영상```
+```ini
+[파이썬 기초 강의]```
+
+https://www.youtube.com/watch?v=yytWGELNeOI
+
+https://www.youtube.com/watch?v=I2yWGzxKKZg&list=WL&index=64
+"""
 
 @client.event
 async def on_message(message):
@@ -45,6 +56,8 @@ async def on_message(message):
         await message.channel.send("тќљчг")
     if message.content == "배고파":
         await message.channel.send("나도")
+    if message.channel.send("공지"):
+        await message.channel.send(announcement2)
 
 access_token = os.environ["BOT_TOKEN"]
 client.run(access_token)
